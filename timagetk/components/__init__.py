61cf6b196f8501df9638fe7575cb827464bdcc6a
jlegrand62 <jlegrand62@gmail.com>
2018-08-28 16:05:48 +0200

    Created a new architecture for the packages by adding submodules
    Moved corresponding files to their submodule.
    No refactoring of those files yet.
    Submodule added:
     - clustering
     - features
     - graphs
     - matplotlib
     - reports
     - segmentation_evaluation
     - signal_quantification
     - visu_3d

--- timagetk/components/__init__.py
+++ timagetk/components/__init__.py
@@ -13,10 +13,10 @@ try:
     from timagetk.components.spatial_image import SpatialImage
 except ImportError:
     raise ImportError('Unable to import SpatialImage.')
-# try:
-#     from timagetk.components.labelled_image import LabelledImage
-# except ImportError:
-#     raise ImportError('Unable to import LabelledImage.')
+try:
+    from timagetk.components.labelled_image import LabelledImage
+except ImportError:
+    raise ImportError('Unable to import LabelledImage.')
 try:
     from timagetk.components.io import imread, imsave
 except ImportError:
