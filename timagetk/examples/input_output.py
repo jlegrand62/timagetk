05d600db8b2a777bb8473695e64f5681478c3098
jlegrand62 <jlegrand62@gmail.com>
2018-09-28 15:51:58 +0200

    Updated Input/Ouput exemple

--- timagetk/examples/input_output.py
+++ timagetk/examples/input_output.py
@@ -1,15 +1,13 @@
 # imports
 import os
-try:
-    from timagetk.components import SpatialImage
-    from timagetk.components import imread, imsave
-    from timagetk.util import data_path
-except ImportError as e:
-    raise ImportError('Import Error: {}'.format(e))
 
-out_path = './results/' # to save results
+from timagetk.components import SpatialImage
+from timagetk.components import imread, imsave
+from timagetk.util import data_path
+
+out_path = './results/'  # to save results
 if not os.path.isdir(out_path):
-    new_fold = os.path.join(os.getcwd(),'results')
+    new_fold = os.path.join(os.getcwd(), 'results')
     os.mkdir(new_fold)
 
 # input image path
@@ -17,37 +15,44 @@ img_path = data_path('time_0_cut.inr')
 # .inr format to SpatialImage
 sp_img = imread(img_path)
 # sp_img is now a SpatialImage instance
-if isinstance(sp_img, SpatialImage):
 
-    # get the SpatialImage metadata, accessor
-    metadata = sp_img.metadata # dictionary
-    # print the SpatialImage metadata
-    print('Metadata :'), metadata
-    # metadata modification
-    metadata['filename'] = img_path
-    # set the SpatialImage metadata, mutator
-    sp_img.set_metadata(metadata)
-    # print the SpatialImage metadata
-    print('New metadata :'), sp_img.metadata
+try:
+    assert isinstance(sp_img, SpatialImage)
+except AssertionError:
+    raise TypeError("Object 'sp_img' is not a SpatialImage.")
 
-    # get the SpatialImage voxelsize (list of floating numbers)
-    vox = sp_img.voxelsize
-    # print the SpatialImage voxelsize
-    print('Voxelsize :'), vox
-    # voxelsize modification
-    new_vox = [] # empty list
-    for ind, val in enumerate(vox):
-        new_vox.append(2.0*val)
-    # set the SpatialImage voxelsize, mutator
-    sp_img.voxelsize = new_vox
-    print('New voxelsize :'), sp_img.voxelsize
+# get the SpatialImage metadata property
+metadata = sp_img.metadata  # dictionary
+# print the SpatialImage metadata
+print('Metadata :'), metadata
+# there are two ways to modify metadata property
+# 1. access the '' metadata directly
+sp_img.metadata['filename'] = img_path
+# print the SpatialImage metadata
+print('New metadata :'), sp_img.metadata
+# 2. update the whole dictionary with a new one:
+sp_img.metadata = {'filename': img_path.split('/')[-1]}
+# print the SpatialImage metadata
+print('New metadata :'), sp_img.metadata
 
-    # output
-    # filename
-    res_name = 'example_input_output.tif'
-    # SpatialImage to .tif format
-    imsave(out_path+res_name, sp_img)
-    # filename
-    res_name = 'example_input_output.inr'
-    # SpatialImage to .inr format
-    imsave(out_path+res_name, sp_img)
+# get the SpatialImage voxelsize (list of floating numbers)
+vox = sp_img.voxelsize
+# print the SpatialImage voxelsize
+print('Voxelsize :'), vox
+# voxelsize modification
+new_vox = []  # empty list
+for ind, val in enumerate(vox):
+    new_vox.append(2.0 * val)
+# set the SpatialImage voxelsize property
+sp_img.voxelsize = new_vox
+print('New voxelsize :'), sp_img.voxelsize
+
+# output
+# filename
+res_name = 'example_input_output.tif'
+# SpatialImage to .tif format
+imsave(out_path + res_name, sp_img)
+# filename
+res_name = 'example_input_output.inr'
+# SpatialImage to .inr format
+imsave(out_path + res_name, sp_img)
